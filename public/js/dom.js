d2d745887c0788cbbab507419b427fb7bff8b0a6
Emma Ogden <ecogden@gmail.com>
2018-08-02 17:42:01 +0100

    added alright styling

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,9 +4,11 @@ var submitBtn = document.querySelector('#search-btn');
 var input = document.querySelector('#search-input');
 var newsContainer = document.querySelector('#news-container');
 var songContainer = document.querySelector('#song-container');
+var sections = document.querySelector('section');
 
 submitBtn.addEventListener('click', function (e) {
   e.preventDefault();
+  sections.classList.add('container-style');
   var inputValue = input.value;
   console.log('input ' + inputValue);
   requestData('/search/' + inputValue, populate);
@@ -19,24 +21,29 @@ function populate(data) {
     var pubDate = data[i]['pubDate'].split('T');
 
     var articleDiv = document.createElement('div');
-    
-    var title = document.createElement('h3');
+
+    // var title = document.createElement('h3');
     var date = document.createElement('p');
     var articleLink = document.createElement('a');
 
     articleDiv.appendChild(articleLink);
-    articleLink.appendChild(title);
     articleDiv.appendChild(date);
 
     var titleContent = document.createTextNode(data[i]['articleTitle']);
     var dateContent = document.createTextNode('Published: ' + pubDate[0]);
     articleLink.setAttribute('href', data[i]['articleUrl']);
-    articleLink.setAttribute('target', '_target');articleDiv.appendChild(date);
+    articleLink.setAttribute('target', '_target'); articleDiv.appendChild(date);
 
-    title.appendChild(titleContent);
+    articleLink.appendChild(titleContent);
     date.appendChild(dateContent);
-    
+
     newsContainer.appendChild(articleDiv);
-    
+
   }
 }
+
+function hideDivs() {
+  if (newsContainer.firstChild) {
+    sections.setAttribute('class', 'visually-hidden');
+  }
+};
