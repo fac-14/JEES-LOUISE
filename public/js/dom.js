fa14e229d6ef9873939294804705bd92f580416d
Emma Ogden <ecogden@gmail.com>
2018-08-02 12:34:54 +0100

    started making populate DOM function

--- public/js/dom.js
+++ public/js/dom.js
@@ -7,5 +7,14 @@ submitBtn.addEventListener('click', function (e) {
   e.preventDefault();
   var inputValue = input.value;
   console.log('input ' + inputValue);
-  requestData('/search/' + inputValue);
+  requestData('/search/' + inputValue, populate);
 });
+
+// unfinished function to populate DOM with guardian response data
+function populate(data) {
+  for (var i = 0; i <= 4; i++) {
+    var title = document.createElement('h3');
+    var date = document.createElement('p');
+    var articleLink = document.createElement('a');
+  }
+}
